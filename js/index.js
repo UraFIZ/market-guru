b4e3203e2b61a8dc43f0bc351d96f3e59b503ebf
Iurii <Iurii@VALENTIN>
2020-02-12 10:48:59 +0200

    working on responseivness

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     getNewTab(".featured-tab",".featured-products .content-wrapper", ".featured-card-row")
+    if( $(window).width() <= 768 && $(window).width() >= 600) {
+        getExtendSearchInput();
+    }else if($(window).width() < 599) {
+        getShowSeachInput()
+    }
+
 })
 
 function getNewTab(btnClass, containerClass, cardClass, func) {
@@ -12,4 +18,32 @@ function getNewTab(btnClass, containerClass, cardClass, func) {
             func();
         }
     });
+}
+
+function getExtendSearchInput() {
+    $("#search").on("click", function(){
+        $(this).animate({
+            "width": "300px"}, 100);
+    })
+    $("#search").on("mouseleave", function(){
+        $(this).animate({
+            "width": "180px"}, 100);
+    })
+   }
+
+function getShowSeachInput() {
+    $(".search-btn").on("click", function(){
+        var logoColumn = $(".logo-col");
+        $(logoColumn).hide();
+        $(".search-col").addClass("col-10");
+        $("#search").animate({
+            "width": "80%"
+        }, 100)
+        $("#search").show();
+    })
+    $(".search-btn").on("mouseleave", function(e){
+        $("#search").hide();
+        $(".search-col").removeClass("col-10");
+        $(".logo-col").show();
+    })
 }
