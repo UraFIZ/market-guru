d1eaffdf124703f989f2c739fdff39bf93d9facb
Iurii <Iurii@VALENTIN>
2020-02-26 19:57:29 +0200

    worked on responsivness

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
 $(document).ready(function(){
-    getNewTab(".featured-tab",".featured-products .content-wrapper", ".featured-card-row")
+    getInitiationOwlCarousalForTinder("#tinder-carousel");
+    getInitiationOwlCarousalNotResp("#respKeyCarousal");
+    //working with lazy loading
+    // getLoadingCardResponsive()
+
+    getClickedCardsInTabs();
+    getNewTab(".featured-tab",".featured-products .content-wrapper", ".featured-card-row", getClickedCardsInTabs)
     if( $(window).width() <= 768 && $(window).width() >= 600) {
         getExtendSearchInputForTab();
     }else if($(window).width() < 599) {
         getShowSearchInput();
-        getInitiationOwlCarousalNotResp("#respKeyCarousal");
     }
     if($(document).width() <= 1024) {
         getInitiationOwlCarousalInOneLine("#how-it-works-row")
     }
 })
 
-function getNewTab(btnClass, containerClass, cardClass) {
+function getNewTab(btnClass, containerClass, cardClass, func) {
     $(btnClass).on('click', function () {
         var btnsIndex = $(this).index()
         $(this)
             .addClass('active').siblings().removeClass('active')
             .closest(containerClass).find(cardClass).removeClass('active').eq(btnsIndex).addClass('active');
+         var innerCards = $(this).closest(containerClass).find(cardClass).eq(btnsIndex).children();
+            if(func) {
+                func(innerCards);
+            }
     });
 }
 
@@ -58,6 +67,7 @@ function getShowSearchInput() {
         }
     })
 }
+
 function getInitiationOwlCarousalInOneLine(id) {
     $(id).addClass("owl-carousel");
     $(id).owlCarousel({
@@ -92,16 +102,198 @@ function getInitiationOwlCarousalInOneLine(id) {
         }
     })
 }
+
 function getInitiationOwlCarousalNotResp(id) {
+    if($(window).width()<=576) {
+        $(id).addClass("owl-carousel");
+        $(id).owlCarousel({
+            loop: false,
+            autoplay: false,
+            nav: true,
+            dots: false,
+            center: false,
+            margin: 0,
+            rewind: false,
+            items: 4,
+            
+        })
+    }
+
+}
+
+function getInitiationOwlCarousalForTinder(id) {
     $(id).addClass("owl-carousel");
     $(id).owlCarousel({
-        loop: false,
+        loop: true,
         autoplay: false,
-        nav: true,
+        nav: false,
         dots: false,
         center: false,
         margin: 0,
         rewind: false,
-        items: 4
+        items: 1,
+        stagePadding: 160,
+        responsive: {
+            1400: {
+                items: 1,
+                stagePadding: 130,
+            },
+            1280: {
+                items: 1,
+                stagePadding: 130,
+            },
+            1024: {
+                items: 1,
+                stagePadding: 80,
+            },
+            768: {
+                items: 1,
+                stagePadding: 80,
+            },
+            600: {
+                items: 1,
+                stagePadding: 40,
+            },
+            470: {
+                items: 1,
+                stagePadding: 30,
+            },
+            360: {
+                items: 1,
+                stagePadding: 30,
+            }
+        }
     })
+}
+
+
+function getLoadedMoreCards(data=[], itemToShow, footerWidth) {
+    $(".show-more").hide();
+    var workingData = checkIsDataAvailable();
+    $(workingData).hide();
+      var step = itemToShow;
+    $(workingData).slice(0, step).show();
+    if(workingData.length < step) {
+        $(".show-more").hide();
+    }
+    $(window).scroll(function() {
+        var { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+        if (scrollTop + clientHeight >= scrollHeight - footerWidth) {
+            if(workingData.length > step ) {
+                step = step + itemToShow;
+                $(".show-more").show();
+                setTimeout(function(){
+                    $(".show-more").hide();
+                }, 1000)
+                $(workingData).slice(0, step).show("slow");
+            }
+        }
+    })
+    // $(btnForShowingMore).on("click", function(){
+    //     if(workingData.length + 5 > step ) {
+    //         step +=step
+    //         $(".show-more-loader").addClass("active");
+    //         setTimeout(function(){
+    //             $(".show-more-loader").removeClass("active");
+    //             $(workingData).slice(0, step).show("slow");
+    //         }, 1500)
+    //     }else{
+    //         $(".show-more-loader").hide();
+    //         $(btnForShowingMore).text("There is no more :(")
+    //         return false
+    //     }
+    // })
+
+}
+
+function isBtnShowMoreDisplayed(data =[]) {
+    $(".show-more").show();
+    if(data.length < 1) {
+        $(".show-more").hide();
+    }
+}
+function getClickedCardsInTabs(data=[]) {
+    isBtnShowMoreDisplayed(data)
+    var workingData = checkIsDataAvailable(data);
+    var likeBtn, dislikeBtn;
+    if($(window).width() >= 768) {
+        likeBtn = $(workingData).map(function(inx, item) {
+            return $(item).find(".featured-like-wrapper .featured-like");
+       })
+        dislikeBtn = $(workingData).map(function(inx, item) {
+           return $(item).find(".featured-like-wrapper .featured-dislike");
+      })
+    }else{
+        likeBtn = $(workingData).map(function(inx, item) {
+            return $(item).find(".respo-voiting-section .featured-like");
+       })
+        dislikeBtn = $(workingData).map(function(inx, item) {
+           return $(item).find(".respo-voiting-section .featured-dislike");
+      })
+    }
+
+
+    $(likeBtn).each(function(inx, item){
+        $(item).click(function(){
+            var itemId = $(item).parents('.col-6').attr("id");
+            $(this).toggleClass("active")
+        })
+    })
+    $(dislikeBtn).each(function(inx, item){
+       
+        $(item).click(function(){
+           var itemId = $(item).parents('.col-6').attr("id");
+            $(this).toggleClass("active")
+        })
+    })
+
+}
+function checkIsDataAvailable(data) {
+    var initialData = $(".featured-card-row.active").children();
+    var workingData = [];
+    if(initialData.length < 1 && data.length < 1) {
+        return false
+    }else if(initialData.length > 1 && data.length < 1) {
+        workingData = $(initialData).map(function(inx, item) {
+            return item
+        })
+    }else if(initialData.length < 1 && data.length > 1) {
+        workingData = $(data).map(function(inx, item) {
+            return item
+        })
+    }else{
+        workingData = $(data).map(function(inx, item) {
+            return item
+        })
+    } 
+    return workingData;
+}
+
+
+function getLoadingCardResponsive(data) {
+    var spaceUselessForScrolling =  $("footer").height() +  $(".how-it-works").outerHeight() -50;
+    if(!data) {
+        if ($(window).width() >= 1200) {
+            getLoadedMoreCards([], 6, spaceUselessForScrolling);
+        } else if ($(window).width() >= 996 && $(window).width() < 1200) {
+            getLoadedMoreCards([], 4, spaceUselessForScrolling);
+            console.log("less 2000")
+        } else if ($(window).width() >= 576 && $(window).width() < 995) {
+            getLoadedMoreCards([], 2, spaceUselessForScrolling);
+        } else {
+            getLoadedMoreCards([], 2, spaceUselessForScrolling);
+        }
+    }else{
+        if ($(window).width() >= 1200) {
+            getLoadedMoreCards(data, 6, spaceUselessForScrolling);
+        } else if ($(window).width() >= 996 && $(window).width() < 1200) {
+            getLoadedMoreCards(data, 4, spaceUselessForScrolling);
+            console.log("less 2000")
+        } else if ($(window).width() >= 576 && $(window).width() < 995) {
+            getLoadedMoreCards(data, 2, spaceUselessForScrolling);
+        } else {
+            getLoadedMoreCards(data, 2, spaceUselessForScrolling);
+        }
+    }
+
 }
